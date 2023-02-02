3dd95b2eaa479207f70de953d4ba75b444086f34
Mistsofnowh3r3 <legobrennan999@gmail.com>
2023-02-02 13:26:01 -0600

    Clean up and refactoring

--- okaybuddy/mainCode.ino
+++ okaybuddy/mainCode.ino
@@ -1,39 +1,35 @@
 #include <Keyboard.h> // using keyboard for now. will do USB controller eventually 
-//need to move to interrupts
 
 //declare globals
-unsigned long startMicros;
-unsigned long currentMicros;
-unsigned long TIMESTART;
-unsigned long TIMEEND;
-
-
 int lastButtonState = 1;    // previous state of the button
 int buttonState = 1;
 // string controller = null; // current controller
 
 //declare constants
+const int button_values[] = {97, 98, 32, KEY_RETURN, KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_LEFT_ARROW, KEY_RIGHT_ARROW, 122, 120, 154, 162};
 const int pulsePin = 1; // the number of the pushbutton pin
 const int latchPin = 2; // the number of the LED pin
 const int dataPin = 3; // the number of the pushbutton pin
 // const int switch =  ; // state of the selection switch
 
 //various mode related things
+const bool nesMode = false; // cureently ditactes if snes or nes
 const bool snesMode = true; // cureently ditactes if snes or nes
 const bool snesMouseMode = true; // cureently ditactes if snes or nes
-const bool edMode = false; // control bindings for ed
+const bool n64Mode = false; // cureently ditactes if snes or nes
 
-void setup() {
-    
-    // set the digital pin STATES
-    pinMode(pulsePin, OUTPUT);
-    pinMode(latchPin, OUTPUT);
-    pinMode(dataPin, INPUT);
+// modes for what the poutput will be 
+const bool edMode = false; // control bindings set to ED compatible
+const bool keyboardMode = true; //output as keyboard
+const bool xinputMode = false; //output as xinput controller
 
-}
 
-void checkButton(int button) {
-    //1 = A 
+
+
+void checkButton(int button, bool mode) {
+
+    // if mode = 0 check for press if 1 check for release
+    //1 = A    
     //2 = B
     //3 = SELECT
     //4 = START
@@ -47,168 +43,42 @@ void checkButton(int button) {
     //10 = X
     //11 = L
     //12 = R
-
-    if (digitalRead(dataPin) == false) // FIRST CHECK IF A KEY IS PRESSED (active low)
-    { 
-        switch(button) {
-
-
-        case 1  : //A 
-            Keyboard.press(97);
-            Serial.print("a");
-            break; 
-        
-        case 2  : //B 
-            Keyboard.press(98);
-            Serial.print("b");
-            break; 
-        
-        case 3  : //SELECT 
-            Keyboard.press(32);
-            Serial.print("SPACE");
-            break; 
-        
-        case 4  : //START 
-            Keyboard.press(KEY_RETURN);
-            Serial.print("ENTER");
-            break; 
-        
-        case 5  : //UP 
-            Keyboard.press(KEY_UP_ARROW);
-            Serial.print("UP");
-            break; 
-        
-        case 6  : //DOWN 
-            Keyboard.press(KEY_DOWN_ARROW);
-            Serial.print("DOWN");
-            break; 
-        
-        case 7  : //LEFT 
-            Keyboard.press(KEY_LEFT_ARROW);
-            Serial.print("LEFT");
-            break; 
-        
-        case 8  : //RIGHT 
-            Keyboard.press(KEY_RIGHT_ARROW);
-            Serial.print("RIGHT");
-            break; 
-        
-        //SNES
-        case 9  : //A
-            if (!snesMode) {
-                break; 
-            }
-            Keyboard.press(122);
-            Serial.print("z");
-            break; 
-        
-        case 10  : //X 
-            if (!snesMode) {
-                break; 
-            }
-            Keyboard.press(120);
-            Serial.print("x");
-            break; 
-        
-        case 11  : //L 
-            if (!snesMode) {
-                break; 
-            }
-            Keyboard.press(154);
-            Serial.print("l");
-            break; 
-        
-        case 12  : //R 
-            if (!snesMode) {
-                break; 
-            }
-            Keyboard.press(162);
-            Serial.print("r");
-            break; 
-        
-
-        default  :  break;
-        }
-    }
-
-}
-
-void checkButtonRelease(int button){
+    // partial chatgpt helped code that makes my original more managable
     
-    if (digitalRead(dataPin) == true) // now check if it was released
-    { 
-        switch(button) {
-        
-        case 1  : //A 
-            Keyboard.release(97);
-            break; 
-        
-        case 2  : //B 
-            Keyboard.release(98);
-            break; 
-        
-        case 3  : //SELECT 
-            Keyboard.release(32);
-            break; 
-        
-        case 4  : //START 
-            Keyboard.release(KEY_RETURN);
-            break; 
-        
-        case 5  : //UP 
-            Keyboard.release(KEY_UP_ARROW);
-            break; 
-        
-        case 6  : //DOWN 
-            Keyboard.release(KEY_DOWN_ARROW);
-            break; 
-        
-        case 7  : //LEFT 
-            Keyboard.release(KEY_LEFT_ARROW);
-            break; 
-        
-        case 8  : //RIGHT 
-            Keyboard.release(KEY_RIGHT_ARROW);
-            break; 
 
-        //SNES
-        case 9  : //A
-            if (!snesMode) {
-                break; 
-            }
-            Keyboard.release(122);
-            Serial.print("z");
-            break; 
-        
-        case 10  : //X 
-            if (!snesMode) {
-                break; 
-            }
-            Keyboard.release(120);
-            Serial.print("x");
-            break; 
-        
-        case 11  : //L 
-            if (!snesMode) {
-                break; 
-            }
-            Keyboard.release(154);
-            Serial.print("l");
-            break; 
-        
-        case 12  : //R 
-            if (!snesMode) {
-                break; 
-            }
-            Keyboard.release(162);
-            Serial.print("r");
-            break; 
-        default  :  break;
-        }
+
+    int button_index = button - 1;
+      
+    if (button_index > 12) { // if we have past all the buttons just return and do nothing
+        return;
     }
+
+    if (!snesMode || button_index < 8) {  // if not in SNES mode or below 8
+        
+        if ((mode == false) && (digitalRead(dataPin) == false)) {
+            Keyboard.press(button_values[button_index]);
+            Serial.print((char) button_values[button_index]);
+            return;
+        }
+        if ((mode == true) && (digitalRead(dataPin) == true)) {
+            Keyboard.release(button_values[button_index]);
+            return;
+        }
+
+    }	
+    // get here if in snes mode if in SNES mode we do the keys past the 8th one
+    if ((mode == false) && (digitalRead(dataPin) == false)) {
+        Keyboard.press(button_values[button_index]);
+        Serial.print((char) button_values[button_index]);
+        return;
+    }
+    if ((mode == true) && (digitalRead(dataPin) == true)) {
+        Keyboard.release(button_values[button_index]);
+        return;
+    }
+
 }
 
-// main program
 void nes() {
     //noInterrupts();
     //interrupts();
@@ -216,8 +86,8 @@ void nes() {
 
 
     digitalWrite(latchPin, HIGH);
-    checkButton(1); // check for A here
-    checkButtonRelease(1);
+    checkButton(1, 0); // check for A here
+    checkButton(1, 1);
 
     delayMicroseconds(12);
 
@@ -233,8 +103,8 @@ void nes() {
         //6 high
         digitalWrite(pulsePin, HIGH);
         // check for the rest of the buttons
-        checkButton(j); 
-        checkButtonRelease(j);
+        checkButton(j, 0); 
+        checkButton(j, 1);
         delayMicroseconds(6);
 
         //6 low
@@ -253,8 +123,8 @@ void snes() {
 
 
     digitalWrite(latchPin, HIGH);
-    checkButton(1); // check for A here
-    checkButtonRelease(1);
+    checkButton(1, false); // check for A here
+    checkButton(1, true);
 
     delayMicroseconds(12);
 
@@ -270,8 +140,8 @@ void snes() {
         //6 high
         digitalWrite(pulsePin, HIGH);
         // check for the rest of the buttons
-        checkButton(j); 
-        checkButtonRelease(j);
+        checkButton(j, false); 
+        checkButton(j, true);
         delayMicroseconds(6);
 
         //6 low
@@ -283,9 +153,17 @@ void snes() {
 
 }  
 
+void setup() {
+    
+    // set the digital pin STATES
+    pinMode(pulsePin, OUTPUT);
+    pinMode(latchPin, OUTPUT);
+    pinMode(dataPin, INPUT);
+
+}
+
 void loop() {
 
-    TIMESTART = micros();
     for ( int t = 1; t < 61; t++){ // try to only do the thing 60 times a second
         snes();
         delayMicroseconds(16550); 
